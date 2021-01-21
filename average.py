97e6d524bd4553ad2b33c14ef8c8c174eea5f738
Nilakash <abc@yahoo.com>
2021-01-21 13:41:19 +0530

    first

---
+++ average.py
@@ -0,0 +1,8 @@
+#In this program we are going to aggregate the marks of two subject and print it
+sub1 = float(input("Enter the marks of 1st subject: ")) #mark of subject 1
+sub2 = float(input("Enter the marks of 2nd subject: ")) #mark of subject 2
+
+avg = (sub1 + sub2)/2 #aggregate operation of two subjects
+
+#printing the aggregate as output
+print("The average marks of given two subject is:",avg)
