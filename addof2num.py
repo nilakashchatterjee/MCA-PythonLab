c162a81e5c36a7d7a5fb3b5ea51f26982cfd3680
Nilakash <abc@yahoo.com>
2021-01-21 12:58:23 +0530

    first

---
+++ addof2num.py
@@ -0,0 +1,6 @@
+#This program demonstrates the addition of two numbers by taking input from the user
+num1 = float(input("Enter the first number: ")) #taking first number
+num2 = float(input("Enter the second number: ")) #taking second number
+add = num1 + num2 #addition operation
+#now printing the output 
+print(f"The Sum of {num1} and {num2} is" add)
