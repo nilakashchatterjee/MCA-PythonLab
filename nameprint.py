070c3b902c655180d32ee8a9c3695aeb9c87b576
Nilakash <abc@yahoo.com>
2021-01-21 12:46:02 +0530

    variable change

--- nameprint.py
+++ nameprint.py
@@ -1,3 +1,3 @@
 #this program will take name from user and prints them as an output
-string = input("Enter your name:")
-print("And your name is",string)
+name = input("Enter your name:")
+print("And your name is",name)
