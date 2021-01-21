4bf5f407a5a6b7bc6075acb141613a2658f679bc
Nilakash <abc@yahoo.com>
2021-01-21 13:01:14 +0530

    first

--- addof2num.py
+++ addof2num.py
@@ -3,4 +3,4 @@ num1 = float(input("Enter the first number: ")) #taking first number
 num2 = float(input("Enter the second number: ")) #taking second number
 add = num1 + num2 #addition operation
 #now printing the output 
-print(f"The Sum of {num1} and {num2} is" add)
+print(f"The Sum of {num1} and {num2} is", add)
