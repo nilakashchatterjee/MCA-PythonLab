3d3644c523acf39d5d5beb12b4f5329bcb3a0cd3
nilakashchatterjee <work.nilakash@gmail.com>
2021-02-11 11:03:23 +0530

    first

--- nameprint.py
+++ nameprint.py
@@ -1,3 +1,3 @@
-#this program will take name from user and prints them as an output
+# In this program will take name from user and prints them as an output
 name = input("Enter your name:")
 print("And your name is",name)
