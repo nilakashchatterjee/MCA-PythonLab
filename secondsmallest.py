31a69d949920f5aea787d3765b6ef4c0feb79aff
nilakashchatterjee <work.nilakash@gmail.com>
2021-02-15 10:09:28 +0530

    second

--- secondsmallest.py
+++ secondsmallest.py
@@ -1,20 +1,15 @@
 num1 = float(input("Enter the first number: ")) 
 num2 = float(input("Enter the second number: "))
 num3 = float(input("Enter the third number: "))
-if num1<num2 and num1<num3:
+if num1<num2<num3 or num3<num2<num1:
+    small= num2
+    print("The second smallest number is:",small)
+elif num2<num3<num1 or num1<num3<num2:
+    small=num3
+    print("The second smallest number is:",small)
+else:
     small=num1
+    print("The second smallest number is:",small)
 
-elif num2<num1 and num2<num3:
-    small = num2
 
-else:
-    small = num3
 
-if num1>small and num1<num3:
-    ssmall = num1
-elif num2>small and num2<num3:
-    ssmall = num2
-else:
-    ssmall = num3
-
-print("The second smallest number is:",ssmall)
