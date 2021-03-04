e3b103911901e0580ba863e3ee272be832e24884
nilakashchatterjee <work.nilakash@gmail.com>
2021-03-04 18:40:58 +0530

    second

--- average2.py
+++ average2.py
@@ -3,8 +3,9 @@ sub1 = float(input("Enter the marks of 1st subject: ")) #marks of subject 1
 sub2 = float(input("Enter the marks of 2nd subject: ")) #marks of subject 2
 sub3 = float(input("Enter the marks of 3rd subject: ")) #marks of subject 3
 sub4 = float(input("Enter the marks of 4th subject: ")) #marks of subject 4
+sub5 = float(input("Enter the marks of 5th subject: ")) #marks of subject 5
 
-avg = (sub1 + sub2 + sub3 + sub4) / 4 #aggregate operation of four subjects
+avg = (sub1 + sub2 + sub3 + sub4 + sub5) / 4 #aggregate operation of four subjects
 
 if avg<40:
     print(f"Your grade is {avg} \nSorry you're failed....")
